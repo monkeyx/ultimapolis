355425f5a016a3a27ae065fc1a5bac3c25a87f81
Seyed Razavi <seyed@educationapps.co.uk>
2015-08-19 16:34:54 +0100

    cache keys

--- app/helpers/cache_helper.rb
+++ app/helpers/cache_helper.rb
@@ -1,4 +1,19 @@
 module CacheHelper
+	def cache_key_for_name(name)
+		return unless name
+		if current_citizen
+			"#{name}-citizen-#{current_citizen.id}-#{current_citizen.updated_at}"
+		elsif current_user
+			"#{name}-#{current_user.id}-user-#{current_user.id}-#{current_user.updated_at}"
+		else
+			name
+		end
+	end
+
+	def cache_key_for_model(model)
+		cache_key_for_name(model ? "#{model.class.name}-#{model.id}-#{model.updated_at}" : 'nil')
+	end
+
 	def cache_key_for_collection(name, collection)
 		return unless collection
 		count = collection.count
