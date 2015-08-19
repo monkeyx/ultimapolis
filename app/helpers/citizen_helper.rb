574dee20fe526937a39769a4df119140f5777164
Seyed Razavi <seyed@educationapps.co.uk>
2015-08-19 17:05:36 +0100

    home buttons

--- app/helpers/citizen_helper.rb
+++ app/helpers/citizen_helper.rb
@@ -4,7 +4,7 @@ module CitizenHelper
 	end
 
 	def current_citizen_path(tab='profile')
-		@current_citizen_path ||= if current_citizen
+		if current_citizen
 			"#{citizen_path(current_citizen)}?tab=#{tab}"
 		else
 			root_url
